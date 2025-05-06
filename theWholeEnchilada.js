5c3209801a4d49f0afa6602e3b59825569d1bfc1
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-05-06 01:02:40 -0400

    Shush dealt with

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -1414,7 +1414,7 @@ export const dailyWordsSmall = [
     'SHADY', 'SHALE', 'SHALT', 'SHARK', 
     'SHEEN', 'SHEEP', 'SHEER', 'SHEIK', 'SHIED', 'SHINY', 'SHIRT',
     'SHOAL', 'SHOCK', 'SHONE', 'SHOOK', 'SHOOT', 'SHORT', 'SHREW',
-    'SHUCK', 'SHUSH', 'SIEVE', 'SIGMA', 'SILKY', 'SINEW', 'SINGE', 'SIREN',
+    'SHUCK', 'SIEVE', 'SIGMA', 'SILKY', 'SINEW', 'SINGE', 'SIREN',
     'SIXTY', 'SKULK', 'SKULL', 'SLACK', 'SLAIN', 'SLANT', 'SLASH', 'SLAVE', 
     'SLEET', 'SLEPT', 'SLICK', 'SLIDE', 'SLIME', 'SLIMY', 'SLING', 'SLINK',
     'SLOOP', 'SLUNK', 'SLURP', 'SLUSH', 'SLYLY', 'SMACK', 'SMELL', 'SMOKY',
@@ -2451,7 +2451,7 @@ export const dailyWordsLarge = [
     'SHOOK', 'SHOOL', 'SHOON', 'SHOOS', 'SHOOT', 'SHOPE', 'SHOPS', 'SHORL', 
     'SHORT', 'SHOTE', 'SHOTS', 'SHOTT', 'SHOWD', 'SHOWS', 
     'SHOYU', 'SHRED', 'SHREW', 'SHRIS', 'SHROW', 'SHTIK', 'SHTUM', 'SHTUP',
-    'SHUCK', 'SHULE', 'SHULN', 'SHULS', 'SHUNS', 'SHURA', 'SHUSH', 'SHUTE', 'SHUTS',
+    'SHUCK', 'SHULE', 'SHULN', 'SHULS', 'SHUNS', 'SHURA', 'SHUTE', 'SHUTS',
     'SHWAS', 'SHYER', 'SIALS', 'SIBBS', 'SIBYL', 'SICES', 'SICHT', 'SICKO', 'SICKS',
     'SICKY', 'SIDAS', 'SIDED', 'SIDER', 'SIDES', 'SIDHA', 'SIDHE', 'SIDLE', 'SIELD',
     'SIENS', 'SIENT', 'SIETH', 'SIEUR', 'SIEVE', 'SIFTS', 'SIGHS', 'SIGIL', 'SIGLA',
@@ -11257,6 +11257,12 @@ export const wordleWords = [
         gameDate: "5/4/25",
         myScore: 5,
         wordNumber: 1415,
+      },
+      {
+        word: "SHUSH",
+        gameDate: "5/5/25",
+        myScore: 3,
+        wordNumber: 1416,
       },
   
   ];
