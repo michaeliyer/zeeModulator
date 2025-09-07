0eb29ee59fe86b34dafb372250693c16ae7a2673
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-09-06 23:00:25 -0400

    Bulge dealt with

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -1,4 +1,5 @@
 
+
 export const fixedWordsLarge = [
     'QQQQQ', 'ZZZZZ', 'JJJJJ', 'QQZZJ', 'KKKJJ', 'JQQZZ', 'JZZQQ', 'QQJZZ', 'ZZJQQ', 'QQJZX', 
     'AAHED', 'AALII', 'AARGH', 'AARTI', 'ABACA', 'ABACI', 'ABACK', 'ABACS', 'ABAFT', 'ABAKA',
@@ -1333,7 +1334,7 @@ export const dailyWordsSmall = [
     'BLOAT', 'BLOOD', 'BLOOM', 'BLUER', 'BLUNT', 'BOBBY', 'BONEY', 
     'BOOTH', 'BOSOM', 'BOTCH', 'BOULE', 'BOUND', 'BOWEL', 
     'BRAWL', 'BRICK', 'BROIL',
-    'BROOD', 'BROWN', 'BRUNT', 'BUDGE', 'BULGE', 'BUNNY', 'BURNT', 'BURST', 'BUSED', 
+    'BROOD', 'BROWN', 'BRUNT', 'BUDGE', 'BUNNY', 'BURNT', 'BURST', 'BUSED', 
     'BUSHY', 'BUTCH', 'BUTTE', 'BUXOM', 'BUYER', 'BYLAW', 'CABAL', 'CABBY', 'CABIN', 'CACTI', 
     'CADDY', 'CAGEY', 'CAIRN', 'CANAL', 
     'CAPUT', 'CASTE', 'CATTY', 'CAVIL', 'CELLO', 'CHAFF', 'CHAIR',
@@ -1350,7 +1351,7 @@ export const dailyWordsSmall = [
     'DEMON', 'DEMUR', 'DENSE', 'DERBY', 'DETOX', 'DEUCE', 'DILLY', 'DIMLY',
     'DIODE', 'DIRTY', 'DITCH', 'DIVER', 'DIZZY', 'DODGY',
     'DOGMA', 'DOUGH', 'DOWDY', 'DOWNY', 'DRAKE', 'DRAMA',
-    'DRANK', 'DRAPE', 'DRAWL', 'DRESS', 'DRIED', 'DRIER', 'DRIFT', 'DRILL',
+    'DRANK', 'DRAPE', 'DRAWL', 'DRESS', 'DRIED', 'DRIER', 'DRILL',
     'DROIT', 'DROSS', 'DROWN', 'DRUID', 'DRUNK', 'DRYLY',
     'DULLY', 'DUMPY', 'DUNCE', 'DUSKY', 'DUSTY', 'DYING',   
     'EATEN', 'EATER', 'ECLAT', 'EERIE', 'EIGHT', 'EKING', 'ELECT',
@@ -1600,7 +1601,7 @@ export const dailyWordsLarge = [
     'BRUTS', 'BUATS', 'BUAZE', 'BUBAL', 'BUBAS', 'BUBBE', 'BUBBY', 'BUBUS', 'BUCHU', 'BUCKO',
     'BUCKS', 'BUCKU', 'BUDAS', 'BUDGE', 'BUDIS', 'BUDOS', 'BUFFA', 'BUFFE', 'BUFFI',
     'BUFFO', 'BUFFS', 'BUFFY', 'BUFOS', 'BUHLS', 'BUHRS', 'BUIKS', 
-    'BUIST', 'BUKES', 'BULBS', 'BULGE', 'BULGY', 'BULKS', 'BULLA', 'BULLS',
+    'BUIST', 'BUKES', 'BULBS', 'BULGY', 'BULKS', 'BULLA', 'BULLS',
     'BULSE', 'BUMBO', 'BUMFS', 'BUMPH', 'BUMPS', 'BUMPY', 'BUNAS', 'BUNCE', 
     'BUNCO', 'BUNDE', 'BUNDH', 'BUNDS', 'BUNDT', 'BUNDU', 'BUNDY', 'BUNGS', 'BUNGY', 'BUNIA',
     'BUNJE', 'BUNJY', 'BUNKO', 'BUNKS', 'BUNNS', 'BUNNY', 'BUNTS', 'BUNTY', 'BUNYA', 'BUOYS',
@@ -1757,7 +1758,7 @@ export const dailyWordsLarge = [
     'DRAGS', 'DRAIL', 'DRAKE', 'DRAMA', 'DRAMS', 'DRANK', 'DRANT', 'DRAPE',
     'DRAPS', 'DRATS', 'DRAVE', 'DRAWL', 'DRAWS', 'DRAYS', 'DREAR',
     'DRECK', 'DREED', 'DREER', 'DREES', 'DREGS', 'DREKS', 'DRENT', 'DRERE', 'DRESS', 'DREST',
-    'DREYS', 'DRIBS', 'DRICE', 'DRIED', 'DRIER', 'DRIES', 'DRIFT', 'DRILL', 'DRILY', 
+    'DREYS', 'DRIBS', 'DRICE', 'DRIED', 'DRIER', 'DRIES', 'DRILL', 'DRILY', 
     'DRIPS', 'DRIPT', 'DROID', 'DROIL', 'DROIT', 'DROKE', 'DROLE', 'DROME',
     'DRONY', 'DROOB', 'DROOG', 'DROOK', 'DROPS', 'DROPT', 'DROSS', 
     'DROUK', 'DROWN', 'DROWS', 'DRUBS', 'DRUGS', 'DRUID', 'DRUMS', 'DRUNK', 'DRUPE', 
@@ -11985,6 +11986,16 @@ export const wordleWords = [
         myScore: 2,
         wordNumber: 1538,
       },  
+      { word: "DRIFT",
+        gameDate: "9/5/25",
+        myScore: 3,
+        wordNumber: 1539,
+      },  
+      { word: "BULGE",
+        gameDate: "9/6/25",
+        myScore: 3,
+        wordNumber: 1540,
+      },  
     
   ];
 
@@ -12001,7 +12012,7 @@ console.log("Push, Stash, and Pop. What the Chryste!")
 console.log("August 4, 2025, Jackwagon. Dickerson. Eff On Off Guyo.")
 console.log("August 7, 2025, CORAL hits! Now what? Scion? Rinse? Caste? But CORAL HITS!")
 console.log("August 9, 2025, Mom: Hello" )
-console.log("September 4, 2025: Trump sux dykk0000! BLEND")
+console.log("September 6, 2025: Trump sux dykk0000! BULGE")
 
 
 console.log(wordleWords)
@@ -12021,4 +12032,3 @@ function logOverallAverageScore() {
 }
 
 logOverallAverageScore();
-
